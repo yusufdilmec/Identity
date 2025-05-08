99f604b952c1aa621f22942ad17c8c53d4489802
yusufdilmec <ydilmec16@gmail.com>
2025-05-08 16:59:31 +0300

    secondCommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GitHubIdentity.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
